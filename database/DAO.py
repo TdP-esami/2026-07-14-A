8572b9e0bd1b3c6f48ada063cb26651723d81172
JosephAv <g.averta3@gmail.com>
2026-07-12 12:04:23 +0200

    first commit

---
+++ database/DAO.py
@@ -0,0 +1,25 @@
+import re
+
+from database.DB_connect import DBConnect
+from model.director import Director
+
+
+class DAO:
+    @staticmethod
+    def getAllMovies():
+        conn = DBConnect.get_connection()
+        results = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """
+                select * from movie m """
+
+        cursor.execute(query)
+
+        for row in cursor:
+            results.append(row)
+
+        cursor.close()
+        conn.close()
+        return results
+
