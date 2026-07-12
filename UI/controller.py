2f2e6acf71c66014d3b79a8c8b0dd8e7d07d6267
JosephAv <g.averta3@gmail.com>
2026-07-12 12:06:38 +0200

    first commit

---
+++ UI/controller.py
@@ -0,0 +1,17 @@
+import flet as ft
+
+
+class Controller:
+    def __init__(self, view, model):
+        self._view = view
+        self._model = model
+
+    def handleCreaGrafo(self, e):
+        pass
+
+    def handleStampaInfo(self, e):
+        pass
+
+    def handleTrovaGruppo(self, e):
+        pass
+
